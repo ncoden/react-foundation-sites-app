02135ce0641e60d1dce688218881431421fd70a1
Nicolas Coden <nicolas@ncoden.fr>
2018-11-13 23:23:45 +0100

    chore: add foundation-sites

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// Foundation JS relies on a global varaible.
+// In ES6, all imports are hoisted to the top of the file
+// so if we used `import` to import Foundation, it would
+// execute earlier than we have assigned the global
+// variable. This is why we have to use CommonJS require()
+// here since it doesn't have the hoisting behavior.
+window.jQuery = $;
+require('foundation-sites');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +21,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+$(document).foundation();
